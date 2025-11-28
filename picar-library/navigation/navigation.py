1a1fe86a5c45bf3b78c5d17a3d3b7f71e10c4686
AlessandroS01 <it_seghini@outlook.com>
2025-11-28 16:26:15 +0100

    Defined angle retrieval according to bbox position

--- picar-library/navigation/navigation.py
+++ picar-library/navigation/navigation.py
@@ -18,49 +18,73 @@ class Navigation:
         Args:
             predictions: Prediction object containing frame and detected checkpoints
         """
-        action = self.decide_action(predictions)
+
+        # TODO implement how to decide which detected object to take into consideration
+        bbox = predictions[0].bounding_box
+        action = self.decide_action(bbox)
 
         if action == Action.FORWARD:
             self.forward()
         elif action == Action.BACKWARD:
             self.backward()
         elif action == Action.LEFT:
-            angle = self.angle_retrieval(action, predictions[0])
+            angle = self.angle_retrieval(bbox)
             self.turn(Action.LEFT, angle)
         elif action == Action.RIGHT:
-            angle = self.angle_retrieval(action, predictions[0])
+            angle = self.angle_retrieval(bbox)
             self.turn(Action.RIGHT, angle)
         self.stop()
 
 
-    def decide_action(self, predictions: list[Prediction]) -> Action:
+    def decide_action(self, bounding_box) -> Action:
         """
             Decides the action to perform based on the predictions
 
         Args:
-            predictions: List of Prediction objects containing frame and detected checkpoints.
+            bounding_box: Checkpoint bounding box
 
         Returns:
             action: Action to perform
         """
-        action = Action.FORWARD
-        # TODO implement how to decide action here
+        x1, y1, x2, y2 = bounding_box
+
+        if x1 < 640 / 2 and x2 < 640 / 2:
+            action = Action.LEFT
+        elif x1 > 640 / 2 and x2 > 640 / 2:
+            action = Action.RIGHT
+        else:
+            action = Action.FORWARD
+
 
         return action
 
-    def angle_retrieval(self, action: Action, prediction: Prediction) -> int:
+    def angle_retrieval(self, bounding_box) -> int:
         """
             Handler for steering angle prediction according to the given action
 
         Args:
-            action: Action to perform
-            prediction: Prediction object containing frame and detected checkpoints.
+            bounding_box: Checkpoint bounding box
 
         Returns:
             angle: Angle in degrees to turn
         """
-        angle = 10
-        # TODO implement how to handle angle creation here
+        x1, y1, x2, y2 = bounding_box
+
+        # LEFT
+        if x1 < 640 / 8:
+            angle = -35
+        elif x1 < 640 / 4:
+            angle = -20
+        elif x1 < 640 / 2:
+            angle = -10
+        # RIGHT
+        elif x1 > 640 / 8:
+            angle = 35
+        elif x1 > 640 / 4:
+            angle = 20
+        else:
+            angle = 10
+
         return angle
 
     def stop(self):
