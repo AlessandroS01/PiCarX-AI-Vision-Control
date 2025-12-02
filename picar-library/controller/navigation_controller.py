98d13222a0fe9f4c0cc63dc6336ea21fe8b2e6c6
AlessandroS01 <it_seghini@outlook.com>
2025-12-02 16:59:45 +0100

    Implemented safe stopping and selection criteria

--- picar-library/controller/navigation_controller.py
+++ picar-library/controller/navigation_controller.py
@@ -13,4 +13,4 @@ class NavigationController:
             predictions: List of prediction checkpoints containing bounding box, confidence and label.
             selected_checkpoint: The checkpoint the user has selected to find.
         """
-        self.navigation.perform_action(predictions)
+        self.navigation.perform_action(predictions, selected_checkpoint)
