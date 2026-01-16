7ece44c471e6b2c3cbc45601c6bdf1b6d2ae77b4
AlessandroS01 <it_seghini@outlook.com>
2026-01-16 17:28:44 +0100

    rotation debugging

--- picar-library/hardware/movement.py
+++ picar-library/hardware/movement.py
@@ -21,7 +21,7 @@ class Movement:
         """Moves the car forward"""
         print("Moving forward")
         self.picar.forward(self.distance)
-        time.sleep(0.01)
+        time.sleep(0.25)
         self.set_servo_angle(0)
 
     def backward(self):
